51bcc5a0668ffb32950f12328f66753bc5bcb134
AnthonyxDecock <anthonyxdecock@gmail.com>
2018-09-15 18:36:09 +0200

    Setup website core

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="Content-type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="css/eventdrop.css">
+<title>Eventdrop - Drop your event</title>
+</head>
+
+<body>
+</body>
+
+</html>
